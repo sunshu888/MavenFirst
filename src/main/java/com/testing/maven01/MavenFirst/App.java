f62a38b203f2576c4693b2ad2658ab5926df28d8
user.name“git config --global user.name user.n <383679640@qq.com>
2020-07-15 16:04:47 +0800

    715提交
    Signed-off-by: user.name“git config --global user.name user.n <383679640@qq.com>

---
+++ src/main/java/com/testing/maven01/MavenFirst/App.java
@@ -0,0 +1,13 @@
+package com.testing.maven01.MavenFirst;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
